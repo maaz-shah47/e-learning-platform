3e923d95afa098b553cc0b5270b06c339f8526f3
Maaz Shah <dev@192.168.10.7>
2022-08-13 21:27:59 +0500

    added friendly gem

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,11 +1,14 @@
 class Course < ApplicationRecord
+  extend FriendlyId
+
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 5 }
 
-  belongs_to :user
+  friendly_id :title, use: :slugged
   def to_s
     title
   end
 
+  belongs_to :user
   has_rich_text :description
 end
