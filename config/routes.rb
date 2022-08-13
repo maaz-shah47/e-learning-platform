9782eecc41a1b797791f53aa5c08ac2b323a41d2
Maaz Shah <dev@192.168.10.7>
2022-08-13 23:46:38 +0500

    devise trackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
+  resources :users
   resources :courses
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
