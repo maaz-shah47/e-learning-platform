0d16f736d11e6568c845e8870542ceb2a6690f97
Maaz Shah <dev@192.168.10.11>
2022-08-15 01:57:56 +0500

    added pundit auth for lessons

---
+++ app/policies/lesson_policy.rb
@@ -0,0 +1,32 @@
+class LessonPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    @user.has_role?(:admin) || @record.course.user == @user
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    @record.course.user == @user
+  end
+
+  def new?
+    # create?
+  end
+
+  def create?
+    # @user.has_role?(:teacher)
+  end
+
+  def destroy?
+    @record.course.user == @user
+  end
+end
