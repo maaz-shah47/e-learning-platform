56c465432cb86dfcca136b91d2368bd96c3fcdab
Maaz Shah <dev@192.168.10.11>
2022-08-14 19:32:56 +0500

    added rich text for lesson content

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,6 +5,8 @@ class Lesson < ApplicationRecord
   validates :content, presence: true, length: { minimum: 5 }
   validates :course, presence: true
 
+  has_rich_text :content
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
