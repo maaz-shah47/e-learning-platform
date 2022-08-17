2973dbff9df07bb1d35f29c18034dcb8c7d1096d
Maaz Shah <dev@192.168.10.8>
2022-08-18 01:50:10 +0500

    button to review course

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -2,16 +2,28 @@ module CoursesHelper
   def enrollment_button(course)
     if current_user
       if course.user == current_user
-        link_to "You created this course. View analytics", course_path(course)
+        link_to 'You created this course. View analytics', course_path(course)
       elsif course.enrollments.where(user_id: current_user.id).any?
-        link_to "You bought this course. Keep learning", course_path(course)
+        link_to 'You bought this course. Keep learning', course_path(course)
       elsif course.price > 0
         link_to number_to_currency(course.price), new_course_enrollment_path(course), class: 'btn btn-success'
       else
-        link_to "Free", new_course_enrollment_path(course), class: 'btn btn-success'
+        link_to 'Free', new_course_enrollment_path(course), class: 'btn btn-success'
       end
     else
-      link_to "Check Price", new_course_enrollment_path(course), class: "btn btn-warning btn-sm"
+      link_to 'Check Price', new_course_enrollment_path(course), class: 'btn btn-warning btn-sm'
+    end
+  end
+
+  def review_button(course)
+    user_course = course.enrollments.where(user: current_user)
+    if current_user && user_course.any?
+      if user_course.pending_review.any?
+        link_to 'Write a review', edit_enrollment_path(user_course.first), class: 'btn btn-success'
+      else
+        'You already reviewed this course'
+        link_to 'View review', enrollment_path(user_course.first)
+      end
     end
   end
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -7,22 +7,18 @@ class Enrollment < ApplicationRecord
   validates :user_id, uniqueness: { scope: :course_id }
   validates :course_id, uniqueness: { scope: :user_id }
 
-  validate :cant_subscribe_to_own_course  #user can't create a subscription if course.user == current_user.id
+  validate :cant_subscribe_to_own_course  # user can't create a subscription if course.user == current_user.id
 
+  scope :pending_review, -> { where(rating: [0, nil, ''], review: [0, nil, '']) }
   def to_s
-    user.to_s + " " + course.to_s
+    user.to_s + ' ' + course.to_s
   end
 
   protected
 
   def cant_subscribe_to_own_course
-    if self.new_record?
-      if self.user_id.present?
-        if self.user_id == course.user_id
-          errors.add(:base, "You can not subscribe to your own course")
-        end
-      end
+    if new_record? && user_id.present? && (user_id == course.user_id)
+      errors.add(:base, 'You can not subscribe to your own course')
     end
   end
-
 end
